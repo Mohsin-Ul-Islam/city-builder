983687fd2fe4a5211e0087d7d74721896299da88
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2022-02-26 15:27:32 +0500

    :art: add sfml boilerplate code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use sfml::window::{Event, Style};
+use sfml::graphics::{RenderWindow, RenderTarget, Color};
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut window = RenderWindow::new(
+        (800, 400),
+        "City Builder",
+        Style::CLOSE,
+        &Default::default(),
+    );
+
+    window.set_framerate_limit(60);
+
+    loop {
+        match window.poll_event() {
+            Some(event) => match event {
+                Event::Closed => return window.close(),
+                _ => (), 
+            },
+            None => continue,
+        }
+        window.clear(Color::WHITE);
+        window.display();
+    }
 }
