d22ca4997fbaa9e7cfe03c0e71381ede863860c9
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2022-03-30 22:48:14 +0500

    feat: add zoom in/out on mouse scroll in/out

--- src/constants.rs
+++ src/constants.rs
@@ -1,2 +1,3 @@
 pub const SCREEN_WIDTH: i32 = 1920; // playable screen width in pixels
 pub const SCREEN_HEIGHT: i32 = 1080; // playable screen height in pixels
+pub const ASPECT_RATIO: f32 = 16.0 / 9.0;
