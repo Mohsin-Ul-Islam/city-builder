93d6c8e76e48072c76d39ecede9269b1966bbd14
Syed Mohsin Ul Islam <47140121+Mohsin-Ul-Islam@users.noreply.github.com>
2022-03-30 21:07:26 +0500

    fix: linting, build & ci errors (#15)

--- src/map.rs
+++ src/map.rs
@@ -4,12 +4,10 @@ use std::io::BufReader;
 use std::path::Path;
 
 #[derive(Debug, Deserialize)]
-pub struct Map {
-    rows: Vec<Vec<i32>>,
-}
+pub struct Map {}
 
 impl Map {
-    pub fn from_json_file(filename: &str) -> Self {
+    pub fn from_json_file(filename: &str) -> Vec<Vec<i32>> {
         serde_json::from_reader(BufReader::new(File::open(Path::new(filename)).unwrap())).unwrap()
     }
 }
--- src/tilemap.rs
+++ src/tilemap.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 
 #[derive(Debug, Deserialize)]
 pub struct TileMapConfig {
-    image_path: String,
+    // image_path: String,
     tiles: Vec<TileMetadata>,
 }
 
@@ -13,13 +13,17 @@ impl TileMapConfig {
     pub fn from_json_file(filename: &str) -> Self {
         serde_json::from_reader(BufReader::new(File::open(Path::new(filename)).unwrap())).unwrap()
     }
+
+    pub fn at(&self, tile_id: &i32) -> &TileMetadata {
+        self.tiles.iter().find(|&tile| tile.id == *tile_id).unwrap()
+    }
 }
 
 #[derive(Debug, Deserialize)]
 pub struct TileMetadata {
-    top: i32,
-    left: i32,
-    name: i32,
-    width: i32,
-    height: i32,
+    pub id: i32,
+    pub top: i32,
+    pub left: i32,
+    pub width: i32,
+    pub height: i32,
 }
